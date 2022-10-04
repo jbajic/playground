fce55c92190c8bd93149bf2056a1c980a3f297dd
jbajic <jure.bajic94@gmail.com>
2022-10-04 22:32:02 +0200

    Add another method

--- cci/linked_lists/palindrome.cpp
+++ cci/linked_lists/palindrome.cpp
@@ -1,9 +1,35 @@
 // Palindrome: Implement a function to check if a linked list is a palindrome.
 #include <iostream>
 #include <list>
+#include <stack>
 
 #include "cci/utils.hpp"
 
+// TODO Solve by reversing pointers, cannot use std list
+
+// single linked list with stack
+bool IsPalindrome2(const auto& list) {
+    std::stack<int> elems;
+    size_t i{0};
+    bool is_odd = list.size() % 2 != 0;
+    for(auto current_elem:list) {
+        if(i < list.size() / 2) {
+            elems.push(current_elem);
+        } else if (is_odd && i == list.size() / 2) {
+          ++i;
+          continue;
+        } else {
+          if (current_elem != elems.top()) {
+            return false;
+          }
+            elems.pop();
+        }
+        ++i;
+    }
+    return true;
+}
+
+// doubly linked list
 bool IsPalindrome(const auto& list) {
     auto it1 = list.begin();
     auto it2 = std::prev(list.end());
@@ -27,6 +53,12 @@ int main() {
     std::cout << static_cast<int>(IsPalindrome(l2)) << std::endl;
     std::cout << static_cast<int>(IsPalindrome(l3)) << std::endl;
     std::cout << static_cast<int>(IsPalindrome(l4)) << std::endl;
+
+    std::cout << std::endl;
+    std::cout << static_cast<int>(IsPalindrome2(l1)) << std::endl;
+    std::cout << static_cast<int>(IsPalindrome2(l2)) << std::endl;
+    std::cout << static_cast<int>(IsPalindrome2(l3)) << std::endl;
+    std::cout << static_cast<int>(IsPalindrome2(l4)) << std::endl;
 
     return 0;
 }
