58aeca1c49a295c37bc5127f3d49f1a1b89c4937
jbajic <jure.bajic94@gmail.com>
2022-10-05 21:55:00 +0200

    Add single linked list variation

--- cci/linked_lists/palindrome.cpp
+++ cci/linked_lists/palindrome.cpp
@@ -5,7 +5,54 @@
 
 #include "cci/utils.hpp"
 
-// TODO Solve by reversing pointers, cannot use std list
+template<typename T>
+struct Node {
+  Node(T data) : data{std::move(data)} {}
+
+  T data;
+  Node<T>* next{nullptr};
+};
+
+template<typename T>
+void PushFront(Node<T>** head, T data) {
+  Node<T>* new_node = new Node<T>(std::move(data));
+  new_node->next = *head;
+  *head = new_node;
+}
+
+template <typename T>
+void PrintSinglyLinkedList(Node<T>* head) {
+  auto* current = head;
+  while (current != nullptr) {
+    std::cout << current->data << ", ";
+    current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+template <typename T>
+bool IsPalindrome3(Node<T>* head) {
+  Node<T>* reverse_head{nullptr};
+
+  // Reverse list
+  auto* current_head = head;
+  while (current_head != nullptr) {
+    auto* new_node = new Node(current_head->data);
+    new_node->next = reverse_head;
+    reverse_head = new_node;
+    current_head = current_head->next;
+  }
+  // Check if palindrome
+  current_head = head;
+  while(current_head != nullptr) {
+    if(current_head->data != reverse_head->data) {
+      return false;
+    }
+    current_head = current_head->next;
+    reverse_head = reverse_head->next;
+  }
+  return true;
+}
 
 // single linked list with stack
 bool IsPalindrome2(const auto& list) {
@@ -49,6 +96,11 @@ int main() {
     std::list<int> l3{1, 2, 2, 1};
     std::list<int> l4{1, 2, 3, 1};
 
+   std::cout << "STD lists" << std::endl;
+    PrintContainer(l1);
+    PrintContainer(l2);
+    PrintContainer(l3);
+    PrintContainer(l4);
     std::cout << static_cast<int>(IsPalindrome(l1)) << std::endl;
     std::cout << static_cast<int>(IsPalindrome(l2)) << std::endl;
     std::cout << static_cast<int>(IsPalindrome(l3)) << std::endl;
@@ -59,6 +111,29 @@ int main() {
     std::cout << static_cast<int>(IsPalindrome2(l2)) << std::endl;
     std::cout << static_cast<int>(IsPalindrome2(l3)) << std::endl;
     std::cout << static_cast<int>(IsPalindrome2(l4)) << std::endl;
+    std::cout << std::endl;
 
+    std::cout << "Singly linked lists" << std::endl;
+    Node<int>* head1 = new Node<int>(1);
+    PushFront(&head1, 2);
+    PushFront(&head1, 3);
+    PushFront(&head1, 4);
+    PrintSinglyLinkedList(head1);
+    Node<int>* head2 = new Node<int>(1);
+    PushFront(&head2, 2);
+    PushFront(&head2, 2);
+    PushFront(&head2, 1);
+    PrintSinglyLinkedList(head2);
+    Node<int>* head3 = new Node<int>(1);
+    PushFront(&head3, 2);
+    PushFront(&head3, 3);
+    PushFront(&head3, 2);
+    PushFront(&head3, 1);
+    PrintSinglyLinkedList(head3);
+
+    std::cout << std::endl;
+    std::cout << static_cast<int>(IsPalindrome3<int>(head1)) << std::endl;
+    std::cout << static_cast<int>(IsPalindrome3<int>(head2)) << std::endl;
+    std::cout << static_cast<int>(IsPalindrome3<int>(head3)) << std::endl;
     return 0;
 }
