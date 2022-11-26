f49916ce4341a2709d7af332b4419bfdf87e1df4
jbajic <jure.bajic94@gmail.com>
2022-11-26 22:17:32 +0100

    Add benchmarking

--- structures/list/benchmark_list.cpp
+++ structures/list/benchmark_list.cpp
@@ -2,7 +2,7 @@
 
 #include "structures/list/list.hpp"
 
-static void BM_BenchmarkInsert(benchmark::State& state) {
+static void BM_BenchmarkInsertPushBack(benchmark::State& state) {
   // Perform setup here
   ladida::structures::List<int64_t> list;
   for (auto _ : state) {
@@ -12,9 +12,65 @@ static void BM_BenchmarkInsert(benchmark::State& state) {
     }
   }
 }
-// Register the function as a benchmark
-BENCHMARK(BM_BenchmarkInsert)
-    ->Arg(1000);
+
+static void BM_BenchmarkInsertPushFront(benchmark::State& state) {
+  // Perform setup here
+  ladida::structures::List<int64_t> list;
+  for (auto _ : state) {
+    // This code gets timed
+    for (auto j{0}; j < state.range(0); ++j) {
+      list.PushFront(j);
+    }
+  }
+}
+
+static void BM_BenchmarkPopFront(benchmark::State& state) {
+  // Perform setup here
+  ladida::structures::List<int64_t> list;
+  for (auto j{0}; j < state.range(0); ++j) {
+    list.PushBack(j);
+  }
+  for (auto _ : state) {
+    // This code gets timed
+    for (auto j{0}; j < state.range(0); ++j) {
+      list.PopFront();
+    }
+  }
+}
+
+static void BM_BenchmarkPopBack(benchmark::State& state) {
+  // Perform setup here
+  ladida::structures::List<int64_t> list;
+  for (auto j{0}; j < state.range(0); ++j) {
+    list.PushBack(j);
+  }
+  for (auto _ : state) {
+    // This code gets timed
+    for (auto j{0}; j < state.range(0); ++j) {
+      list.PopBack();
+    }
+  }
+}
+
+BENCHMARK(BM_BenchmarkInsertPushBack)
+    ->Arg(1000)
+    ->Arg(10000)
+    ->Arg(100000)
+    ->Arg(1000000)
+    ->Arg(10000000)
+    ->Arg(100000000);
+
+BENCHMARK(BM_BenchmarkInsertPushFront)
+    ->Arg(1000)
+    ->Arg(10000)
+    ->Arg(100000)
+    ->Arg(1000000)
+    ->Arg(10000000)
+    ->Arg(100000000);
+
+BENCHMARK(BM_BenchmarkPopFront)->Arg(100000000);
+
+BENCHMARK(BM_BenchmarkPopBack)->Arg(100000000);
 
 // Run the benchmark
 BENCHMARK_MAIN();
