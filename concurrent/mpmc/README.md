2006b17f9ef83634dfd9cbe02cc1f8f036d31b8d
Jure Bajic <jure.bajic94@gmail.com>
2024-05-17 09:21:53 +0200

    Add readme

--- concurrent/mpmc/README.md
+++ concurrent/mpmc/README.md
@@ -8,3 +8,9 @@ cmake -DCMAKE_BUILD_TYPE=Release ..
 make
 sudo ./benchmark/mpmc_bench --benchmark_repetitions=3 --benchmark_perf_counters="CYCLES,INSTRUCTIONS"
 ```
+
+## Observations:
+* Using random in multihreaded testing environment is actually bad since random itself
+takes a lock on a global state causing additional overhead when testing mutex
+overheads. (https://ashvardanian.com/posts/google-benchmark/#hardware-performance-counters)
+
--- concurrent/mpmc/benchmark/mpmc.cpp
+++ concurrent/mpmc/benchmark/mpmc.cpp
@@ -4,16 +4,25 @@
 #include <condition_variable>
 #include <cstddef>
 #include <mutex>
-#include <random>
 #include <thread>
 #include <vector>
 
-constexpr auto COUNTER_WORK = 100000;
+constexpr auto COUNTER_WORK = 1000000;
 
 template <typename T>
-struct LockedQueue {
-  LockedQueue(size_t capacity) : max_capacity(capacity) {
-    queue.reserve(capacity);
+struct MutexQueue {
+  MutexQueue() {}
+
+  MutexQueue(size_t max_capacity) : max_capacity(max_capacity) {
+    queue.reserve(max_capacity);
+  }
+
+  void Append(T num) { queue.push_back(num); }
+
+  T Pop() {
+    auto elem = queue.back();
+    queue.pop_back();
+    return elem;
   }
 
   std::mutex mtx;
@@ -45,7 +54,6 @@ class UnboundedQueue {
   std::vector<T> queue;
   size_t max_capacity;
 };
-
 
 template <typename T>
 class BoundedQueue {
@@ -79,8 +87,7 @@ class BoundedQueue {
   size_t max_capicity;
 };
 
-static void BM_MPMC_BoundedMutex(
-    benchmark::State &state) {
+static void BM_MPMC_UnboundedMutex(benchmark::State &state) {
   size_t threads = std::thread::hardware_concurrency() / 2;
 
   assert(threads % 2 == 0);
@@ -90,24 +97,15 @@ static void BM_MPMC_BoundedMutex(
     producers.reserve(threads);
     std::vector<std::thread> consumers;
     consumers.reserve(threads);
-    LockedQueue<int> queue(state.range(0));
+    MutexQueue<int> queue;
 
     for (size_t i = 0; i < threads; ++i) {
       producers.emplace_back([&queue]() {
-        std::random_device r;
-        std::default_random_engine e1(r());
-        std::uniform_int_distribution<int> uniform_dist(0, 1000);
-
         auto counter = COUNTER_WORK;
         while (counter != 0) {
-          {
-            auto guard = std::lock_guard<std::mutex>(queue.mtx);
-            if (queue.queue.size() < queue.max_capacity) {
-              auto num = uniform_dist(e1);
-              queue.queue.push_back(num);
-            }
-            --counter;
-          }
+          auto guard = std::lock_guard<std::mutex>(queue.mtx);
+          queue.Append(counter);
+          --counter;
         }
       });
     }
@@ -117,7 +115,7 @@ static void BM_MPMC_BoundedMutex(
         while (counter != 0) {
           auto guard = std::lock_guard<std::mutex>(queue.mtx);
           if (!queue.queue.empty()) {
-            queue.queue.pop_back();
+            queue.Pop();
           }
           --counter;
         }
@@ -134,8 +132,57 @@ static void BM_MPMC_BoundedMutex(
   }
 }
 
-static void BM_MPMC_Unbounded(
-    benchmark::State &state) {
+static void BM_MPMC_BoundedMutex(benchmark::State &state) {
+  size_t threads = std::thread::hardware_concurrency() / 2;
+
+  assert(threads % 2 == 0);
+
+  for (auto _ : state) {
+    std::vector<std::thread> producers;
+    producers.reserve(threads);
+    std::vector<std::thread> consumers;
+    consumers.reserve(threads);
+    MutexQueue<int> queue(state.range(0));
+
+    for (size_t i = 0; i < threads; ++i) {
+      producers.emplace_back([&queue]() {
+        auto counter = COUNTER_WORK;
+        while (counter != 0) {
+          {
+            auto guard = std::lock_guard<std::mutex>(queue.mtx);
+            if (queue.queue.size() < queue.max_capacity) {
+              auto num = counter;
+              queue.Append(num);
+            }
+            --counter;
+          }
+        }
+      });
+    }
+    for (size_t i = 0; i < threads; ++i) {
+      consumers.emplace_back([&queue]() {
+        auto counter = COUNTER_WORK;
+        while (counter != 0) {
+          auto guard = std::lock_guard<std::mutex>(queue.mtx);
+          if (!queue.queue.empty()) {
+            queue.Pop();
+          }
+          --counter;
+        }
+      });
+    }
+
+    // Wrap up
+    for (auto &producer_thread : producers) {
+      producer_thread.join();
+    }
+    for (auto &consumer_thread : consumers) {
+      consumer_thread.join();
+    }
+  }
+}
+
+static void BM_MPMC_Unbounded(benchmark::State &state) {
   size_t threads = std::thread::hardware_concurrency() / 2;
   assert(threads % 2 == 0);
 
@@ -150,13 +197,9 @@ static void BM_MPMC_Unbounded(
     state.ResumeTiming();
     for (size_t i = 0; i < threads; ++i) {
       producers.emplace_back([&queue]() {
-        std::random_device r;
-        std::default_random_engine e1(r());
-        std::uniform_int_distribution<int> uniform_dist(0, 1000);
-
         auto counter = COUNTER_WORK;
         while (counter != 0) {
-          queue.Append(uniform_dist(e1));
+          queue.Append(counter);
           counter--;
         }
       });
@@ -181,8 +224,7 @@ static void BM_MPMC_Unbounded(
   }
 }
 
-static void BM_MPMC_Bounded(
-    benchmark::State &state) {
+static void BM_MPMC_Bounded(benchmark::State &state) {
   size_t threads = std::thread::hardware_concurrency() / 2;
   assert(threads % 2 == 0);
 
@@ -197,13 +239,9 @@ static void BM_MPMC_Bounded(
     state.ResumeTiming();
     for (size_t i = 0; i < threads; ++i) {
       producers.emplace_back([&queue]() {
-        std::random_device r;
-        std::default_random_engine e1(r());
-        std::uniform_int_distribution<int> uniform_dist(0, 1000);
-
         auto counter = COUNTER_WORK;
         while (counter != 0) {
-          queue.Append(uniform_dist(e1));
+          queue.Append(counter);
           counter--;
         }
       });
@@ -228,17 +266,23 @@ static void BM_MPMC_Bounded(
   }
 }
 
-BENCHMARK(BM_MPMC_BoundedMutex)
-    ->RangeMultiplier(10)
-    ->Range(10, 1000)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_MPMC_UnboundedMutex)->Unit(benchmark::kMillisecond);
 
 BENCHMARK(BM_MPMC_BoundedMutex)
     ->RangeMultiplier(10)
-    ->Range(10, 1000)->Unit(benchmark::kMillisecond)
+    ->Range(10, 1000)
+    ->Unit(benchmark::kMillisecond);
+
+BENCHMARK(BM_MPMC_BoundedMutex)
+    ->RangeMultiplier(10)
+    ->Range(10, 1000)
+    ->Unit(benchmark::kMillisecond)
     ->MeasureProcessCPUTime();
 
 BENCHMARK(BM_MPMC_Unbounded)->Unit(benchmark::kMillisecond);
-BENCHMARK(BM_MPMC_Unbounded)->Unit(benchmark::kMillisecond)->MeasureProcessCPUTime();
+BENCHMARK(BM_MPMC_Unbounded)
+    ->Unit(benchmark::kMillisecond)
+    ->MeasureProcessCPUTime();
 
 BENCHMARK(BM_MPMC_Bounded)
     ->RangeMultiplier(10)
